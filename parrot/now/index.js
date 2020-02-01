44ca78bf6359abb966467c1dad83f4809aac5fc6
KhanMisha <mishakhan2000@mail.ru>
2020-02-01 12:56:57 +0300

    qq

--- parrot/now/index.js
+++ parrot/now/index.js
@@ -17,7 +17,7 @@ module.exports = async (req, res) => {
                 // В свойстве response.text возвращается исходная реплика пользователя.
                 // Если навык был активирован без дополнительной команды,
                 // пользователю нужно сказать "Hello!".
-                text: request.original_utterance || 'Hello!',
+                text: request.original_utterance || 'haha!',
 
                 // Свойство response.end_session возвращается со значением false,
                 // чтобы диалог не завершался.
